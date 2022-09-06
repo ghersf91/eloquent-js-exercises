66eb015a80cccdd37a81af4a465b5de0ff9f4e62
Ghers Fisman <ghersf91@gmail.com>
2022-09-06 19:13:15 +0200

    exercise 4.2 done

--- index.js
+++ index.js
@@ -27,9 +27,9 @@ let chessBoard = '';
 for (let i = 0; i < size; i++) {
   for (let j = 0; j < size; j++) {
     if ((j + i) % 2 == 0) {
-    	chessBoard += ' ';
+      chessBoard += ' ';
     } else {
-    	chessBoard += '#';
+      chessBoard += '#';
     }
   }
   chessBoard += '\n';
@@ -42,44 +42,44 @@ console.log(chessBoard)
 
 // 3.1 Minimum
 
-let min = function(x, y) {
-	if (x < y) {
-    	return x;
-    } else if (y < x) {
-    	return y;
-    } else {
-    	return 'x and y are equal';
-    }
+let min = function (x, y) {
+  if (x < y) {
+    return x;
+  } else if (y < x) {
+    return y;
+  } else {
+    return 'x and y are equal';
+  }
 }
 
 // 3.2 Recursion
 
 isEven = x => {
-	if (x == 0) {
-    	return true;
-    } else if (x == 1) {
-      	return false;
-    } else if (x < 0) {
-      	return isEven(-x);
-    } else {
-    	return isEven(x - 2);
-    }
+  if (x == 0) {
+    return true;
+  } else if (x == 1) {
+    return false;
+  } else if (x < 0) {
+    return isEven(-x);
+  } else {
+    return isEven(x - 2);
+  }
 }
 
 // 3.3 Bean counting
 
 countChar = (string, char) => {
-  	let numberChar = 0;
-	for ( i = 0; i < string.length; i++) {
-    	if (string[i] === char) {
-        	numberChar++;
-        }
+  let numberChar = 0;
+  for (i = 0; i < string.length; i++) {
+    if (string[i] === char) {
+      numberChar++;
     }
-  	return numberChar;
+  }
+  return numberChar;
 }
 
 countBs = string => {
-	return countChar(string, 'B');
+  return countChar(string, 'B');
 }
 
 
@@ -90,7 +90,7 @@ countBs = string => {
 function range(start, end) {
   let arr = [];
   let curr = start;
-  while(arr.length < end - start + 1) {
+  while (arr.length < end - start + 1) {
     arr.push(curr);
     curr++;
   }
@@ -109,15 +109,50 @@ function rangeStep(start, end, step = 1) {
   let arr = [];
   let curr = start;
   if (start < end) {
-    while(arr.length < (end - start + 1) / Math.abs(step)) {
-    arr.push(curr);
-    curr = curr + step;
+    while (arr.length < (end - start + 1) / Math.abs(step)) {
+      arr.push(curr);
+      curr = curr + step;
     }
   } else if (start > end) {
-    while(arr.length < (start - end + 1) / Math.abs(step)) {
-    arr.push(curr);
-    curr = curr + step;
+    while (arr.length < (start - end + 1) / Math.abs(step)) {
+      arr.push(curr);
+      curr = curr + step;
     }
   }
   return arr;
 }
+
+// 4.2 Reversing an array
+
+function reverseArray(arr) {
+  let revArr = [];
+  for (let elem of arr) {
+    revArr.unshift(elem);
+  }
+  return revArr;
+}
+
+function reverseArrayInPlace(arr) {
+  let revArr = [];
+  for (let elem of arr) {
+    revArr.unshift(elem);
+  }
+  arr.splice(0, arr.length, ...revArr);
+  return arr;
+}
+
+// Book answer to second part of the exercise (more elegant solution with swap)
+// function reverseArrayInPlace(arr) {
+//   for (i = 0; i < Math.floor(arr.length / 2); i++) {
+//     let old = arr[i];
+//     arr[i] = arr[arr.length - 1 - i];
+//     arr[arr.length - 1 - i] = old;
+//   }
+//   return arr;
+// }
+
+// 4.3 A list
+
+
+
+// 4.4 Deep comparison
