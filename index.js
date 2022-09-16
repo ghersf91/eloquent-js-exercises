d5af6b6068661d5574a74c2f0115015d31c178bf
Ghers Fisman <ghersf91@gmail.com>
2022-09-16 22:33:52 +0200

    added book solutions to exercises 4.3 and 4.4 which i couldnt solve on my own

--- index.js
+++ index.js
@@ -153,6 +153,53 @@ function reverseArrayInPlace(arr) {
 
 // 4.3 A list
 
+// Book answer
+// function arrayToList(array) {
+//   let list = null;
+//   for (let i = array.length - 1; i >= 0; i--) {
+//     list = { value: array[i], rest: list };
+//   }
+//   return list;
+// }
 
+// function listToArray(list) {
+//   let array = [];
+//   for (let node = list; node; node = node.rest) {
+//     array.push(node.value);
+//   }
+//   return array;
+// }
+
+// function prepend(value, list) {
+//   return { value, rest: list };
+// }
+
+// function nth(list, n) {
+//   if (!list) return undefined;
+//   else if (n == 0) return list.value;
+//   else return nth(list.rest, n - 1);
+// }
 
 // 4.4 Deep comparison
+
+// Book answer
+// function deepEqual(a, b) {
+//   if (a === b) return true;
+
+//   if (a == null || typeof a != "object" ||
+//     b == null || typeof b != "object") return false;
+
+//   let keysA = Object.keys(a), keysB = Object.keys(b);
+
+//   if (keysA.length != keysB.length) return false;
+
+//   for (let key of keysA) {
+//     if (!keysB.includes(key) || !deepEqual(a[key], b[key])) return false;
+//   }
+
+//   return true;
+// }
+
+
+// Chapter 4
+
